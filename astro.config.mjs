322815a2a24c03db0154d73a32db219428930a91
Hyprocritor <i@xsx.st>
2024-11-14 05:47:23 +0000

    disable image compression for better performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,173 +1,177 @@
+import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import swup from '@swup/astro'
 import Compress from 'astro-compress'
+import expressiveCode from 'astro-expressive-code'
 import icon from 'astro-icon'
-import {defineConfig} from 'astro/config'
+import { defineConfig } from 'astro/config'
 import Color from 'colorjs.io'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeComponents from 'rehype-components'/* Render the custom directive content */
+import rehypeCallouts from 'rehype-callouts'
+import rehypeComponents from 'rehype-components' /* Render the custom directive content */
 import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug'
-import remarkDirective from 'remark-directive'/* Handle directives */
+import remarkDirective from 'remark-directive' /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from 'remark-github-admonitions-to-directives'
-import remarkMath from 'remark-math'
-import {AdmonitionComponent} from './src/plugins/rehype-component-admonition.mjs'
-import {GithubCardComponent} from './src/plugins/rehype-component-github-card.mjs'
-import {parseDirectiveNode} from './src/plugins/remark-directive-rehype.js'
-import {remarkExcerpt} from './src/plugins/remark-excerpt.js'
-import {remarkReadingTime} from './src/plugins/remark-reading-time.mjs'
-import mdx from "@astrojs/mdx";
-import rehypeCallouts from "rehype-callouts";
 import remarkLinkCard from 'remark-link-card'
-import {defaultFootnoteBackContent} from "./src/plugins/remarkRehypeFootnoteBackContent.mjs";
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
-import expressiveCode from 'astro-expressive-code';
+import remarkMath from 'remark-math'
+import { AdmonitionComponent } from './src/plugins/rehype-component-admonition.mjs'
+import { GithubCardComponent } from './src/plugins/rehype-component-github-card.mjs'
+import { parseDirectiveNode } from './src/plugins/remark-directive-rehype.js'
+import { remarkExcerpt } from './src/plugins/remark-excerpt.js'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import { defaultFootnoteBackContent } from './src/plugins/remarkRehypeFootnoteBackContent.mjs'
 
 const oklchToHex = str => {
-    const DEFAULT_HUE = 250
-    const regex = /-?\d+(\.\d+)?/g
-    const matches = str.string.match(regex)
-    const lch = [matches[0], matches[1], DEFAULT_HUE]
-    return new Color('oklch', lch).to('srgb').toString({
-        format: 'hex',
-    })
+  const DEFAULT_HUE = 250
+  const regex = /-?\d+(\.\d+)?/g
+  const matches = str.string.match(regex)
+  const lch = [matches[0], matches[1], DEFAULT_HUE]
+  return new Color('oklch', lch).to('srgb').toString({
+    format: 'hex',
+  })
 }
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://23h.at/',
-    base: '/',
-    trailingSlash: 'always',
-    integrations: [
+  site: 'https://23h.at/',
+  base: '/',
+  trailingSlash: 'always',
+  integrations: [
+    tailwind(),
+    swup({
+      theme: false,
+      animationClass: 'transition-swup-', // see https://swup.js.org/options/#animationselector
+      // the default value `transition-` cause transition delay
+      // when the Tailwind class `transition-all` is used
+      containers: ['main', '#toc'],
+      smoothScrolling: true,
+      cache: true,
+      preload: true,
+      progress: true,
+      accessibility: true,
+      updateHead: true,
+      updateBodyClass: false,
+      globalInstance: true,
+    }),
+    icon({
+      include: {
+        'material-symbols': ['*'],
+        'fa6-brands': ['*'],
+        'fa6-regular': ['*'],
+        'fa6-solid': ['*'],
+      },
+    }),
+    svelte(),
+    sitemap(),
+    expressiveCode({
+      defaultLocale: 'zh-CN',
+      defaultProps: {
+        wrap: true,
+      },
+      plugins: [pluginLineNumbers()],
+      themes: ['slack-ochin', 'slack-dark'],
+      frames: {
+        extractFileNameFromCode: true,
+        showCopyToClipboardButton: true,
+      },
+    }),
+    mdx({
+      remarkRehype: {
+        footnoteLabel: '注释',
+        footnoteBackContent: defaultFootnoteBackContent,
+      },
+      optimize: true,
+    }),
+    Compress({
+      CSS: true,
+      Image: false,
 
-        tailwind(), swup({
-        theme: false,
-        animationClass: 'transition-swup-',   // see https://swup.js.org/options/#animationselector
-                                              // the default value `transition-` cause transition delay
-                                              // when the Tailwind class `transition-all` is used
-        containers: ['main', '#toc'],
-        smoothScrolling: true,
-        cache: true,
-        preload: true,
-        progress: true,
-        accessibility: true,
-        updateHead: true,
-        updateBodyClass: false,
-        globalInstance: true,
-    }), icon({
-        include: {
-            'material-symbols': ['*'],
-            'fa6-brands': ['*'],
-            'fa6-regular': ['*'],
-            'fa6-solid': ['*'],
+      JavaScript: true,
+      HTML: true,
+      Action: {
+        Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
+      },
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkMath,
+      remarkReadingTime,
+      remarkExcerpt,
+      remarkGithubAdmonitionsToDirectives,
+      remarkLinkCard,
+      remarkDirective,
+      parseDirectiveNode,
+    ],
+    rehypePlugins: [
+      rehypeCallouts,
+      rehypeKatex,
+      rehypeSlug,
+      [
+        rehypeComponents,
+        {
+          components: {
+            github: GithubCardComponent,
+            note: (x, y) => AdmonitionComponent(x, y, 'note'),
+            tip: (x, y) => AdmonitionComponent(x, y, 'tip'),
+            important: (x, y) => AdmonitionComponent(x, y, 'important'),
+            caution: (x, y) => AdmonitionComponent(x, y, 'caution'),
+            warning: (x, y) => AdmonitionComponent(x, y, 'warning'),
+          },
         },
-    }),svelte(), sitemap(),
-        expressiveCode({
-            defaultLocale: 'zh-CN',
-            defaultProps:{
-              wrap:true,
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            className: ['anchor'],
+          },
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['anchor-icon'],
+              'data-pagefind-ignore': true,
             },
-            plugins:[pluginLineNumbers()],
-            themes: ['slack-ochin', 'slack-dark'],
-            frames: {
-                extractFileNameFromCode:true,
-                showCopyToClipboardButton:true
-            },
-        }),
-        mdx({
-        remarkRehype: {
-            footnoteLabel:"注释",
-            footnoteBackContent:defaultFootnoteBackContent
-
-        },
-        optimize:false,
-
-
-    }), Compress({
-        CSS: true,
-        Image: true,
-        Action: {
-            Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
-        },
-    })],
-    markdown: {
-        remarkPlugins: [
-            remarkMath,
-            remarkReadingTime,
-            remarkExcerpt,
-            remarkGithubAdmonitionsToDirectives,
-            remarkLinkCard,
-            remarkDirective,
-            parseDirectiveNode,
-        ],
-        rehypePlugins: [
-            rehypeCallouts,
-            rehypeKatex,
-            rehypeSlug,
-            [
-                rehypeComponents,
-                {
-                    components: {
-                        github: GithubCardComponent,
-                        note: (x, y) => AdmonitionComponent(x, y, 'note'),
-                        tip: (x, y) => AdmonitionComponent(x, y, 'tip'),
-                        important: (x, y) => AdmonitionComponent(x, y, 'important'),
-                        caution: (x, y) => AdmonitionComponent(x, y, 'caution'),
-                        warning: (x, y) => AdmonitionComponent(x, y, 'warning'),
-                    },
-                },
+            children: [
+              {
+                type: 'text',
+                value: '#',
+              },
             ],
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: 'append',
-                    properties: {
-                        className: ['anchor'],
-                    },
-                    content: {
-                        type: 'element',
-                        tagName: 'span',
-                        properties: {
-                            className: ['anchor-icon'],
-                            'data-pagefind-ignore': true,
-                        },
-                        children: [
-                            {
-                                type: 'text',
-                                value: '#',
-                            },
-                        ],
-                    },
-                },
-            ],
-
-        ],
-    },
-    vite: {
-        build: {
-            rollupOptions: {
-                onwarn(warning, warn) {
-                    // temporarily suppress this warning
-                    if (
-                        warning.message.includes('is dynamically imported by') &&
-                        warning.message.includes('but also statically imported by')
-                    ) {
-                        return
-                    }
-                    warn(warning)
-                },
-            },
+          },
         },
-        css: {
-            preprocessorOptions: {
-                stylus: {
-                    define: {
-                        oklchToHex: oklchToHex,
-                    },
-                },
-            },
+      ],
+    ],
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          // temporarily suppress this warning
+          if (
+            warning.message.includes('is dynamically imported by') &&
+            warning.message.includes('but also statically imported by')
+          ) {
+            return
+          }
+          warn(warning)
         },
+      },
     },
+    css: {
+      preprocessorOptions: {
+        stylus: {
+          define: {
+            oklchToHex: oklchToHex,
+          },
+        },
+      },
+    },
+  },
 })
